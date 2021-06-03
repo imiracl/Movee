5f94d42eeb51c2864358bed62deaa64dc09aabeb
imiracl <38769648+imiracl@users.noreply.github.com>
2021-06-03 14:19:33 +0300

    qwer

--- app/js/app.js
+++ app/js/app.js
@@ -9,18 +9,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
+//	$('#nav-menu').toggleClass('isactive');
 
 })
 
 let Myhamburger = document.getElementById("Myhamburger");
+let nav_menu = document.getElementById("nav-menu");
 console.log(Myhamburger);
 
 Myhamburger.addEventListener("click" , ()=>{
 	if (Myhamburger.classList.contains('is-active')){
 		Myhamburger.classList.remove("is-active");
+		nav_menu.classList.remove("is-active");
+		nav_menu.classList.remove("open-menu");
+
 	}
 	else{
 		Myhamburger.classList.add("is-active");
+		nav_menu.classList.add("is-active");
+		nav_menu.classList.add("open-menu");
 	}
 	
 })
