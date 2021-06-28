2913297b28513c4e1108ba92c60021985cded4dd
imiracl <38769648+imiracl@users.noreply.github.com>
2021-06-28 14:51:48 +0300

    ergergerg

--- app/js/app.js
+++ app/js/app.js
@@ -6,33 +6,47 @@
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Custom JS
-//	$('#nav-menu').toggleClass('isactive');
-
-})
+document.addEventListener("DOMContentLoaded", () => {
+  // Custom JS
+  //	$('#nav-menu').toggleClass('isactive');
+});
 
 let Myhamburger = document.getElementById("Myhamburger");
 let nav_menu = document.getElementById("nav-menu");
 let body = document.getElementById("boody");
 console.log(body);
 
-Myhamburger.addEventListener("click" , ()=>{
-	if (Myhamburger.classList.contains('is-active')){
-		Myhamburger.classList.remove("is-active");
-		nav_menu.classList.remove("is-active");
-		nav_menu.classList.remove("open-menu");
-		body.classList.remove("hide_scroll");
+Myhamburger.addEventListener("click", () => {
+  if (Myhamburger.classList.contains("is-active")) {
+    Myhamburger.classList.remove("is-active");
+    nav_menu.classList.remove("is-active");
+    nav_menu.classList.remove("open-menu");
+    body.classList.remove("hide_scroll");
+  } else {
+    Myhamburger.classList.add("is-active");
+    nav_menu.classList.add("is-active");
+    nav_menu.classList.add("open-menu");
+    body.classList.add("hide_scroll");
+  }
+});
 
-	}
-	else{
-		Myhamburger.classList.add("is-active");
-		nav_menu.classList.add("is-active");
-		nav_menu.classList.add("open-menu");
-		body.classList.add("hide_scroll");
+let allPrices = document.getElementById("all__prices__hide");
+let Cards = document.getElementsByClassName("card_hidden");
+
+allPrices.addEventListener("click", () => {
+  // for ( element of Cards){
+
+  // 	element.classList.toggle('card_hidden');
+
+  // }
+  for (let index = 0; index < Cards.length; index++) {
 		
-	}
-	
-})
+		while (index < Cards.length ) {
+			
+			Cards[index].classList.toggle("card_hidden");
+			
+		}
+  }
+});
+
 
