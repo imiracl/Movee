87248f6f53e222369b0ad029f7d0a5c1789c099d
imiracl <38769648+imiracl@users.noreply.github.com>
2021-10-25 17:11:16 +0300

    fix some bags
    .....

--- app/js/app.js
+++ app/js/app.js
@@ -130,3 +130,39 @@ function ListTable() {
 ListTable();
 initSwiper();
 
+
+
+
+
+
+
+
+
+function Tabs() {
+  var bindAll = function() {
+    var menuElements = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < menuElements.length ; i++) {
+      menuElements[i].addEventListener('click', change, false);
+    }
+  }
+
+  var clear = function() {
+    var menuElements = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < menuElements.length ; i++) {
+      menuElements[i].classList.remove('active');
+      var id = menuElements[i].getAttribute('data-tab');
+      document.getElementById(id).classList.remove('active');
+    }
+  }
+
+  var change = function(e) {
+    clear();
+    e.target.classList.add('active');
+    var id = e.currentTarget.getAttribute('data-tab');
+    document.getElementById(id).classList.add('active');
+  }
+
+  bindAll();
+}
+
+var connectTabs = new Tabs();
